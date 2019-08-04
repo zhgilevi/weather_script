4909b5bfe2dfac8b9a79ed99206d77e79cffa5ef
zhgilevi <38855124+zhgilevi@users.noreply.github.com>
2019-08-04 12:51:41 +0500

    Update script.py

--- script.py
+++ script.py
@@ -1,2 +1,12 @@
-while True:
-	print("I am working!")
+import telebot
+token='781229574:AAGC6K39EQ1VNcf2RTOlLpXg_KWoHPAZTIk'
+bot= telebot.TeleBot(token=token)
+
+
+
+@bot.message_handler(commands=['start','get'])
+def send_answer(message):
+    bot.reply_to(message,'Helooo')
+
+
+bot.polling()
